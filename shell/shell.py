6b0fcd58aecbe531258573d29a3cdacb88bd252f
alejandro <a.ortega6243@gmail.com>
2020-09-15 18:28:18 -0600

    getting shell started with current path

--- shell/shell.py
+++ shell/shell.py
@@ -1,17 +1,29 @@
-#alex ortega
+#! /user/bin/env python3
+
 import os, sys, re
 
 class Shell:
 
     def __init__(self):
         self.pid = os.getpid()
-        print("process: ", self.pid)
+        self.executeCommand()
+
+    def executeCommand(self):        
+        print("process:",self.pid)
+        #command prompt
         while 1:
-            command = input()
-            command.strip()
+            path = os.getcwd()
+            command = input(path + "\n$ ")
             if command == "exit":
                 break
-            print("command: ", command)
+            tokens = command.split()
+            print(tokens)
 
+        """
+        newProcess = os.fork()
+        if newProcess < 0:
+            os.write(2, ("Fork failed: (%d\n" % newProcess).encode())
+            sys.exit(1)
+            """
 
 run = Shell()
