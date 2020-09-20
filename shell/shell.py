d75c6d87e41dc0084472fd57eec83a09947e6185
alejandro <a.ortega6243@gmail.com>
2020-09-20 10:21:10 -0600

    checking code for none working features

--- shell/shell.py
+++ shell/shell.py
@@ -29,6 +29,17 @@ class Shell:
                 else:
                     print(command+": command not found")
 
+    def exeCommand(self, command):
+        args = command.split()
+        for dir in re.split(":", os.environ['PATH']): #try each directory in the path
+            program = "%s/%s" % (dir, args[0])
+            try:
+                os.execve(program, args, os.environ) #try to exec program
+            except FileNotFoundError:                #this is expected
+                pass                                 #fail quietly                
+        os.write(2, ("Command %s not found. Try again.\n" % args[0]).encode())
+        sys.exit(1)                                  #terminate with error
+
     def changeDirectory(self, path):
         try:
             os.chdir(path)
@@ -53,13 +64,13 @@ class Shell:
             if '>' in args:
                 redirect = command.split('> ')
                 os.close(1)
-                os.open(redirect[1], os.O_CREAT | os.O_WRONLY);
+                os.open(redirect[1], os.O_CREAT | os.O_WRONLY)
                 os.set_inheritable(1, True)
                 self.exeCommand(redirect[0])
             if '<' in args:
                 redirect = command.split('< ')
                 os.close(0)
-                os.open(redirect[1], os.O_RDONLY);
+                os.open(redirect[1], os.O_RDONLY)
                 os.set_inheritable(0, True)
                 self.exeCommand(redirect[0])
             self.exeCommand(args)
@@ -73,9 +84,10 @@ class Shell:
         for f in (pr, pw):
             os.set_inheritable(f, True)
 
+        #newProcess = 0
         newProcess = os.fork()
         if newProcess < 0:
-            print("fork failed, returning %d\n" %rc, file=sys.stderr)
+            print("fork failed, returning %d\n" %newProcess, file=sys.stderr)
             sys.exit(1)
         elif newProcess == 0:
             os.close(1)         #redirect child's stdout
@@ -91,16 +103,5 @@ class Shell:
             for fd in (pw, pr):
                 os.close(fd)
             self.exeCommand(command[command.index('|') + 1:])
-
-    def exeCommand(self, command):
-        args = command.split()
-        for dir in re.split(":", os.environ['PATH']): #try each directory in the path
-            program = "%s/%s" % (dir, args[0])
-            try:
-                os.execve(program, args, os.environ) #try to exec program
-            except FileNotFoundError:                #this is expected
-                pass                                 #fail quietly                
-        os.write(2, ("Command %s not found. Try again.\n" % args[0]).encode())
-        sys.exit(1)                                  #terminate with error
         
 run = Shell()
