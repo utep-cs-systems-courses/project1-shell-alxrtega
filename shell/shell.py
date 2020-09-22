eed6eeaf05a7deeaa2ec4c2c353ee4bac92660c4
alejandro <a.ortega6243@gmail.com>
2020-09-22 13:17:46 -0600

    os.read finally works, still need to work on redirection

--- shell/shell.py
+++ shell/shell.py
@@ -1,6 +1,6 @@
 import os, sys, re
 '''
-@author Alejandro Ortega
+@Author Alejandro Ortega
 class Shell will emulate a bash terminal
 '''
 class Shell:
@@ -17,10 +17,8 @@ class Shell:
         #command prompt
         while 1:
 
-            currentPath = os.getcwd()
-            print(currentPath + "\n$", end = " ")
             command = self.getUserInput()
-            
+
             if command == "exit":
                 sys.exit(0)
             elif ">" in command or "<" in command:
@@ -35,7 +33,13 @@ class Shell:
                 pass
 
     def getUserInput(self):
-        userInput = input()
+        currentPath = os.getcwd()
+        print(currentPath + "\n$ ", end = " ")
+        userInput = os.read(0, 128)
+        userInput = userInput.decode().split("\n")
+        userInput = userInput[0]
+        userInput = str(userInput)
+        
         return userInput
 
     '''
@@ -44,16 +48,30 @@ class Shell:
     to run the desired commands
     '''
     def runCommand(self, command):
-        command = command.split(" ")
-        for dir in re.split(":", os.environ['PATH']):
-            program = "%s/%s" % (dir, command[0])
-            try:
-                os.execve(program, command, os.environ) 
-            except FileNotFoundError:                
-                pass                                 
+        args = command.split()
+        pid  = os.getpid()
 
-        os.write(2, ("Command %s not found. Try again.\n" % command[0]).encode())
-        sys.exit(1)                                  
+        newProcess = os.fork()
+        if newProcess < 0:
+
+            os.write(2,("Fork failed.").encode())
+            sys.exit(1)
+
+        elif newProcess == 0:
+
+            for dir in re.split(":", os.environ['PATH']):
+                program = "%s/%s" % (dir, args[0])
+
+                try:
+                    os.execve(program, args, os.environ) 
+                except FileNotFoundError:                
+                    pass                                 
+
+            os.write(2, ("Command %s not found. Try again.\n" % args[0]).encode())
+            sys.exit(1)                                  
+
+        else: 
+            waiting = os.wait()
 
     '''
     changeDirectory() will manage the directory change
@@ -71,20 +89,35 @@ class Shell:
     are linked by '<' or '>'
     '''
     def redirection(self, command):
-        redirect = command.split('>')
-        if '>' in command:
-            
-            os.close(1)
-            os.open(redirect[1], os.O_CREAT | os.O_WRONLY)
-            os.set_inheritable(1, True)
+        newProcess = os.fork()
 
-        elif '<' in command:
+        if newProcess < 0:
 
-            os.close(0)
-            os.open(redirect[1], os.O_RDONLY)
-            os.set_inheritable(0, True)
+            os.write(2, ("Fork failed, returning %d\n" % newProcess).encode())
+            sys.exit(1)
 
-        self.runCommand(redirect[0])
+        elif newProcess == 0: 
+
+            if '>' in command:
+
+                redirect = command.split('>')
+                os.close(1)
+                os.open(redirect[1], os.O_CREAT | os.O_WRONLY)
+                os.set_inheritable(1, True)
+                self.runCommand(redirect[0])
+
+            if '<' in command:
+
+                redirect = command.split('<')
+                os.close(0)
+                os.open(redirect[1], os.O_RDONLY)
+                os.set_inheritable(0, True)
+                self.runCommand(redirect[0])
+
+            self.runCommand(command)
+        else:
+            if not '&' in command:
+                waiting = os.wait()
 
     '''
     pipeCommand() will manage piping to emulate the terminals by splitting the command
@@ -92,10 +125,12 @@ class Shell:
     '''
     def pipeCommand(self, command):
         cmd1, cmd2 = command.split('|')    
-
         pr, pw = os.pipe()
-        newProcess = os.fork()
 
+        for f in (pr, pw):
+            os.set_inheritable(f, True)
+
+        newProcess = os.fork()
         if newProcess < 0:
 
             os.write(1, "fork failed, returning %d\n" %newProcess)
@@ -117,10 +152,6 @@ class Shell:
             os.set_inheritable(0, True)
             for fd in (pw, pr):
                 os.close(fd)
-
-            if "|" in cmd2:
-                self.pipeCommand(cmd2)
-
             self.runCommand(cmd2)
         
 run = Shell()
